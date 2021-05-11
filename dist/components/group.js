a4f29e45f10f0088c995377b354ff9614ba7da0a
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2021-05-11 16:57:09 -0700

    Adding build code and new babel configuration

---
+++ dist/components/group.js
@@ -0,0 +1,78 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const GroupIcon = () => {
+  return /*#__PURE__*/_react.default.createElement("svg", {
+    className: "icon",
+    viewBox: "0 0 40 40",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, /*#__PURE__*/_react.default.createElement("path", {
+    d: "M10 13.75C12.0711 13.75 13.75 12.0711 13.75 10C13.75 7.92893 12.0711 6.25 10 6.25C7.92893 6.25 6.25 7.92893 6.25 10C6.25 12.0711 7.92893 13.75 10 13.75Z",
+    fill: "#282E3E"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: "M7.3 15C6.02975 15 5 16.0297 5 17.3V28.95C5 30.2203 6.02975 31.25 7.3 31.25H12.7C13.9703 31.25 15 30.2203 15 28.95V17.3C15 16.0297 13.9703 15 12.7 15H7.3Z",
+    fill: "#282E3E"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: "M30 13.75C32.0711 13.75 33.75 12.0711 33.75 10C33.75 7.92893 32.0711 6.25 30 6.25C27.9289 6.25 26.25 7.92893 26.25 10C26.25 12.0711 27.9289 13.75 30 13.75Z",
+    fill: "white"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: "M27.3 15C26.0297 15 25 16.0297 25 17.3V28.95C25 30.2203 26.0297 31.25 27.3 31.25H32.7C33.9703 31.25 35 30.2203 35 28.95V17.3C35 16.0297 33.9703 15 32.7 15H27.3Z",
+    fill: "white"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: "M20 15C22.7614 15 25 12.7614 25 10C25 7.23858 22.7614 5 20 5C17.2386 5 15 7.23858 15 10C15 12.7614 17.2386 15 20 15Z",
+    fill: "url(#group)"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: "M16.25 16.25C14.8693 16.25 13.75 17.3693 13.75 18.75V32.5C13.75 33.8807 14.8693 35 16.25 35H23.75C25.1307 35 26.25 33.8807 26.25 32.5V18.75C26.25 17.3693 25.1307 16.25 23.75 16.25H16.25Z",
+    fill: "url(#group1)"
+  }), /*#__PURE__*/_react.default.createElement("defs", null, /*#__PURE__*/_react.default.createElement("linearGradient", {
+    id: "group",
+    x1: "14.4196",
+    y1: "35",
+    x2: "31.1543",
+    y2: "31.9553",
+    gradientUnits: "userSpaceOnUse"
+  }, /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.109375",
+    stopColor: "#F14D70"
+  }), /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.317708",
+    stopColor: "#FF008A"
+  }), /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.520833",
+    stopColor: "#FF446C"
+  }), /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.869792",
+    stopColor: "#FFB016"
+  })), /*#__PURE__*/_react.default.createElement("linearGradient", {
+    id: "group1",
+    x1: "14.4196",
+    y1: "35",
+    x2: "31.1543",
+    y2: "31.9553",
+    gradientUnits: "userSpaceOnUse"
+  }, /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.109375",
+    stopColor: "#F14D70"
+  }), /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.317708",
+    stopColor: "#FF008A"
+  }), /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.520833",
+    stopColor: "#FF446C"
+  }), /*#__PURE__*/_react.default.createElement("stop", {
+    offset: "0.869792",
+    stopColor: "#FFB016"
+  }))));
+};
+
+var _default = GroupIcon;
+exports.default = _default;
