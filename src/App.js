991a0fb20441ae68ff2f7b19c3eabe1963eee5d6
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2021-05-14 15:58:26 -0700

    Added all icons to App.js to test them before future launches and updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,124 @@
 import './App.css';
 
+import AwardIcon from './lib/award'
+import BookIcon from './lib/book'
+import BottleIcon from './lib/bottle'
+import BriefcaseIcon from './lib/briefcase'
+import CharterLogoDark from './lib/charter-logo-dark'
+import CharterLogoLight from './lib/charter-logo-light'
+import CheckboxIcon from './lib/checkbox'
+import ClockIcon from './lib/clock'
+import ClockIconDark from './lib/clock-dark'
+import ColorsIcon from './lib/colors'
+import CompassIconDark from './lib/compass-dark'
+import CompassIconLight from './lib/compass-light'
+import CrossIcon from './lib/cross'
+import DataIcon from './lib/data'
+import FacebookIconDark from './lib/facebook-dark'
+import FacebookIconLight from './lib/facebook-light'
+import FlowerIcon from './lib/flower'
+import FormIcon from './lib/form'
+import GearsIcon from './lib/gears'
+import GearsIconDark from './lib/gears-dark'
+import GroupIcon from './lib/group'
+import HealthcareIcon from './lib/healthcare'
+import HealthcareIconDark from './lib/healthcare-dark'
+import HeartIcon from './lib/heart'
+import HeroGraphic from './lib/hero-graphic'
+import HomeIcon from './lib/home'
+import HospitalIcon from './lib/hospital'
+import HouseIcon from './lib/house'
+import InstagramIconDark from './lib/instagram-dark'
+import InstagramIconLight from './lib/instagram-light'
+import InternIcon from './lib/intern'
+import LikeIcon from './lib/like'
+import LinkedinIconDark from './lib/linkedin-dark'
+import LinkedinIconLight from './lib/linkedin-light'
+import MedkitIcon from './lib/medkit'
+import MoneybagIcon from './lib/moneybag'
+import NetworkIcon from './lib/network'
+import NetworkIconDark from './lib/network-dark'
+import PhoneIcon from './lib/phone'
+import PillIcon from './lib/pill'
+import PinIcon from './lib/pin'
+import PlusSignIcon from './lib/plus-sign'
+import RulerIcon from './lib/ruler'
+import SearchIcon from './lib/search'
+import ShapesIcon from './lib/shapes'
+import SocialIcon from './lib/social'
+import SquaresIcon from './lib/squares'
+import SquaresIconDark from './lib/squares-dark'
+import StethoscopeIcon from './lib/stethoscope'
+import TargetIcon from './lib/target'
+import TelescopeIcon from './lib/telescope'
+import TwitterIconDark from './lib/twitter-dark'
+import TwitterIconLight from './lib/twitter-light'
+import TypographyIcon from './lib/typography'
+import VisionIcon from './lib/vision'
+import VolunteerIcon from './lib/volunteer'
+import VolunteerIconDark from './lib/volunteer-dark'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AwardIcon   className='icon' />
+        <BookIcon   className='icon' />
+        <BottleIcon   className='icon' />
+        <BriefcaseIcon   className='icon' />
+        <CharterLogoDark  className='icon'  />
+        <CharterLogoLight   className='icon' />
+        <CheckboxIcon   className='icon' />
+        <ClockIcon   className='icon' />
+        <ClockIconDark   className='icon' />
+        <ColorsIcon   className='icon' />
+        <CompassIconDark   className='icon' />
+        <CompassIconLight   className='icon' />
+        <CrossIcon  className='icon'  />
+        <DataIcon  className='icon'  />
+        <FacebookIconDark  className='icon'  />
+        <FacebookIconLight   className='icon' />
+        <FlowerIcon   className='icon' />
+        <FormIcon   className='icon' />
+        <GearsIcon  className='icon'  />
+        <GearsIconDark   className='icon' />
+        <GroupIcon  className='icon'  />
+        <HealthcareIcon   className='icon' />
+        <HealthcareIconDark   className='icon' />
+        <HeartIcon   className='icon' />
+        <HeroGraphic   className='icon' />
+        <HomeIcon   className='icon' />
+        <HospitalIcon className='icon' />
+        <HouseIcon className='icon' />
+        <InstagramIconDark className='icon' />
+        <InstagramIconLight className='icon' />
+        <InternIcon className='icon' />
+        <LikeIcon className='icon' />
+        <LinkedinIconDark className='icon' />
+        <LinkedinIconLight className='icon' />
+        <MedkitIcon className='icon' />
+        <MoneybagIcon className='icon' />
+        <NetworkIcon className='icon' />
+        <NetworkIconDark className='icon' />
+        <PhoneIcon className='icon' />
+        <PillIcon className='icon' />
+        <PinIcon className='icon' />
+        <PlusSignIcon className='icon' />
+        <RulerIcon className='icon' />
+        <SearchIcon className='icon' />
+        <ShapesIcon className='icon' />
+        <SocialIcon className='icon' />
+        <SquaresIcon className='icon' />
+        <SquaresIconDark className='icon' />
+        <StethoscopeIcon className='icon' />
+        <TargetIcon className='icon' />
+        <TelescopeIcon className='icon' />
+        <TwitterIconDark className='icon' />
+        <TwitterIconLight className='icon' />
+        <TypographyIcon className='icon' />
+        <VisionIcon className='icon' />
+        <VolunteerIcon className='icon' />
+        <VolunteerIconDark className='icon' />
       </header>
     </div>
   );
