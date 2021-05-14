2d9f4b6ac8b1d861d36921a1cd9255a227ca724a
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2021-05-14 15:07:34 -0700

    Changed grey icons to dark bg

--- src/lib/squares.js
+++ src/lib/squares.js
@@ -4,30 +4,30 @@ const SquaresIcon = (props) => {
 
 return (
     <svg className={props.className} width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M5.5 3C4.11929 3 3 4.11929 3 5.5V15.5C3 16.8807 4.11929 18 5.5 18H15.5C16.8807 18 18 16.8807 18 15.5V5.5C18 4.11929 16.8807 3 15.5 3H5.5Z" fill="url(#squares)"/>
-      <path d="M5.5 22C4.11929 22 3 23.1193 3 24.5V34.5C3 35.8807 4.11929 37 5.5 37H15.5C16.8807 37 18 35.8807 18 34.5V24.5C18 23.1193 16.8807 22 15.5 22H5.5Z" fill="url(#squares1)"/>
-      <path d="M22 5.5C22 4.11929 23.1193 3 24.5 3H34.5C35.8807 3 37 4.11929 37 5.5V15.5C37 16.8807 35.8807 18 34.5 18H24.5C23.1193 18 22 16.8807 22 15.5V5.5Z" fill="url(#squares2)"/>
-      <path d="M24.5 22C23.1193 22 22 23.1193 22 24.5V34.5C22 35.8807 23.1193 37 24.5 37H34.5C35.8807 37 37 35.8807 37 34.5V24.5C37 23.1193 35.8807 22 34.5 22H24.5Z" fill="url(#squares3)"/>
+      <path d="M7.20588 5C5.98761 5 5 5.98761 5 7.20588V16.0294C5 17.2477 5.98761 18.2353 7.20588 18.2353H16.0294C17.2477 18.2353 18.2353 17.2477 18.2353 16.0294V7.20588C18.2353 5.98761 17.2477 5 16.0294 5H7.20588Z" fill="url(#squares)"/>
+      <path d="M7.20588 21.7647C5.98761 21.7647 5 22.7523 5 23.9706V32.7941C5 34.0124 5.98761 35 7.20588 35H16.0294C17.2477 35 18.2353 34.0124 18.2353 32.7941V23.9706C18.2353 22.7523 17.2477 21.7647 16.0294 21.7647H7.20588Z" fill="url(#squares1)"/>
+      <path d="M21.7647 7.20588C21.7647 5.98761 22.7523 5 23.9706 5H32.7941C34.0124 5 35 5.98761 35 7.20588V16.0294C35 17.2477 34.0124 18.2353 32.7941 18.2353H23.9706C22.7523 18.2353 21.7647 17.2477 21.7647 16.0294V7.20588Z" fill="url(#squares2)"/>
+      <path d="M23.9706 21.7647C22.7523 21.7647 21.7647 22.7523 21.7647 23.9706V32.7941C21.7647 34.0124 22.7523 35 23.9706 35H32.7941C34.0124 35 35 34.0124 35 32.7941V23.9706C35 22.7523 34.0124 21.7647 32.7941 21.7647H23.9706Z" fill="url(#squares3)"/>
       <defs>
-        <linearGradient id="squares" x1="4.82143" y1="37" x2="44.3159" y2="19.7544" gradientUnits="userSpaceOnUse">
+        <linearGradient id="squares" x1="6.60714" y1="35" x2="41.4552" y2="19.7833" gradientUnits="userSpaceOnUse">
           <stop offset="0.109375" stopColor="#F14D70"/>
           <stop offset="0.317708" stopColor="#FF008A"/>
           <stop offset="0.520833" stopColor="#FF446C"/>
           <stop offset="0.869792" stopColor="#FFB016"/>
         </linearGradient>
-        <linearGradient id="squares1" x1="4.82143" y1="37" x2="44.3159" y2="19.7544" gradientUnits="userSpaceOnUse">
+        <linearGradient id="squares1" x1="6.60714" y1="35" x2="41.4552" y2="19.7833" gradientUnits="userSpaceOnUse">
           <stop offset="0.109375" stopColor="#F14D70"/>
           <stop offset="0.317708" stopColor="#FF008A"/>
           <stop offset="0.520833" stopColor="#FF446C"/>
           <stop offset="0.869792" stopColor="#FFB016"/>
         </linearGradient>
-        <linearGradient id="squares2" x1="4.82143" y1="37" x2="44.3159" y2="19.7544" gradientUnits="userSpaceOnUse">
+        <linearGradient id="squares2" x1="6.60714" y1="35" x2="41.4552" y2="19.7833" gradientUnits="userSpaceOnUse">
           <stop offset="0.109375" stopColor="#F14D70"/>
           <stop offset="0.317708" stopColor="#FF008A"/>
           <stop offset="0.520833" stopColor="#FF446C"/>
           <stop offset="0.869792" stopColor="#FFB016"/>
         </linearGradient>
-        <linearGradient id="squares3" x1="4.82143" y1="37" x2="44.3159" y2="19.7544" gradientUnits="userSpaceOnUse">
+        <linearGradient id="squares3" x1="6.60714" y1="35" x2="41.4552" y2="19.7833" gradientUnits="userSpaceOnUse">
           <stop offset="0.109375" stopColor="#F14D70"/>
           <stop offset="0.317708" stopColor="#FF008A"/>
           <stop offset="0.520833" stopColor="#FF446C"/>
